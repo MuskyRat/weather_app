815af416e5a44ce3f05b9d8dc35457232744077b
Musky_Rat <avalancherocking@gmail.com>
2022-01-16 16:02:40 +0300

    commit 4

--- src/components/Main/DailyForecastInfo.tsx
+++ src/components/Main/DailyForecastInfo.tsx
@@ -16,7 +16,7 @@ const DailyForecastInfo: React.FC = React.memo( () => {
             {data && <div className={style.headerContainer}>
                 <h1 className={style.header}>7 days Daily Forecast</h1>
             </div>}
-            {data && <div className={style.itemsContainer}>{
+            {data && <div className={style.itemsContainer}><ul className={style.list}>{
                 data.filter((e, index) => index >= 0 && index < 7).map((e, index) => {
 
                     let date = new Date((timezoneOffset ? e.dt + timezoneOffset : e.dt) * 1000);
@@ -27,56 +27,60 @@ const DailyForecastInfo: React.FC = React.memo( () => {
 
                     const src = `http://openweathermap.org/img/wn/${iconName}@2x.png`
 
-                    return  <div key={index} className={style.itemContainer}>
+                    return  <li key={index} className={style.itemContainer}>
                                 <div className={style.item}>
                                     <div className={style.date}>{forecastDate}</div>
                                     <div className={style.descriptionContainer}>
                                         <div className={style.description}>{`${e.weather[0].description[0].toUpperCase()}${e.weather[0].description.slice(1)}`}</div>
                                         <div className={style.iconContainer}><img className={style.icon} src={src || icon} alt="icon" /></div>
                                     </div>
-                                    <div className={style.tempNamesInfo}>
-                                        <div className={style.tempName}>Temperature:</div>
-                                        <div className={style.tempName}>Feels like:</div>
-                                        <div className={style.tempName992}>Temp:</div>
-                                        <div className={style.tempName992}>Feels:</div>
+                                    <div className={style.temperaturesBlock}>
+                                        <div className={style.tempNamesInfo}>
+                                            <div className={style.tempName}>Temperature:</div>
+                                            <div className={style.tempName}>Feels like:</div>
+                                            <div className={style.tempName992}>Temp:</div>
+                                            <div className={style.tempName992}>Feels:</div>
+                                        </div>
+                                        <div className={style.tempContainer}>
+                                            <div className={style.timeName}>Morning</div>
+                                            <div className={style.temperature}>{`${Math.round(e.temp.morn)} C`}</div>
+                                            <div className={style.feelsLike}>{`${Math.round(e.feels_like.morn)} C`}</div>
+                                        </div>
+                                        <div className={style.tempContainer}>
+                                            <div className={style.timeName}>Day</div>
+                                            <div className={style.temperature}>{`${Math.round(e.temp.day)} C`}</div>
+                                            <div className={style.feelsLike}>{`${Math.round(e.feels_like.day)} C`}</div>
+                                        </div>
+                                        <div className={style.tempContainer}>
+                                            <div className={style.timeName}>Evening</div>
+                                            <div className={style.temperature}>{`${Math.round(e.temp.eve)} C`}</div>
+                                            <div className={style.feelsLike}>{`${Math.round(e.feels_like.eve)} C`}</div>
+                                        </div>
+                                        <div className={style.tempContainer}>
+                                            <div className={style.timeName}>Night</div>
+                                            <div className={style.temperature}>{`${Math.round(e.temp.night)} C`}</div>
+                                            <div className={style.feelsLike}>{`${Math.round(e.feels_like.night)} C`}</div>
+                                        </div>
                                     </div>
-                                    <div className={style.tempContainer}>
-                                        <div className={style.timeName}>Morning</div>
-                                        <div className={style.temperature}>{`${Math.round(e.temp.morn)} C`}</div>
-                                        <div className={style.feelsLike}>{`${Math.round(e.feels_like.morn)} C`}</div>
-                                    </div>
-                                    <div className={style.tempContainer}>
-                                        <div className={style.timeName}>Day</div>
-                                        <div className={style.temperature}>{`${Math.round(e.temp.day)} C`}</div>
-                                        <div className={style.feelsLike}>{`${Math.round(e.feels_like.day)} C`}</div>
-                                    </div>
-                                    <div className={style.tempContainer}>
-                                        <div className={style.timeName}>Evening</div>
-                                        <div className={style.temperature}>{`${Math.round(e.temp.eve)} C`}</div>
-                                        <div className={style.feelsLike}>{`${Math.round(e.feels_like.eve)} C`}</div>
-                                    </div>
-                                    <div className={style.tempContainer}>
-                                        <div className={style.timeName}>Night</div>
-                                        <div className={style.temperature}>{`${Math.round(e.temp.night)} C`}</div>
-                                        <div className={style.feelsLike}>{`${Math.round(e.feels_like.night)} C`}</div>
-                                    </div>
-                                    <div className={style.propContainer}>
-                                        <div className={`${style.name} ${style.windName}`}>Wind speed</div>
-                                        <div className={style.wind}>{`${Math.round(e.wind_speed)} m/s`}</div>
-                                    </div>
-                                    <div className={style.propContainer}>
-                                        <div className={style.name}>Humidity</div>
-                                        <div className={style.humidity}>{`${Math.round(e.humidity)} %`}</div>
-                                    </div>
-                                    <div className={`${style.propContainer} ${style.pressurePropContainer}`}>
-                                        <div className={style.name}>Pressure</div>
-                                        <div className={style.pressure}>{`${Math.round(e.pressure / 1.33322390232)} mm Hg`}</div>
+                                    <div className={style.propsBlock}>
+                                        <div className={style.propContainer}>
+                                            <div className={`${style.name} ${style.windName}`}>Wind speed</div>
+                                            <div className={style.wind}>{`${Math.round(e.wind_speed)} m/s`}</div>
+                                        </div>
+                                        <div className={style.propContainer}>
+                                            <div className={style.name}>Humidity</div>
+                                            <div className={style.humidity}>{`${Math.round(e.humidity)} %`}</div>
+                                        </div>
+                                        <div className={`${style.propContainer} ${style.pressurePropContainer}`}>
+                                            <div className={style.name}>Pressure</div>
+                                            <div className={style.pressure}>{`${Math.round(e.pressure / 1.33322390232)} mm Hg`}</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </li>
 
                 })
-            }</div> }
+            }</ul></div> }
         </div>
 
     )
